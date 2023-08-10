6f1b849225e65a599528bed4ab3986c5953396fb
Morgan Ney <morganney@gmail.com>
2023-08-10 18:04:45 -0500

    ci: deploy. (#3)

--- demo.ts
+++ demo.ts
@@ -2,7 +2,7 @@ import { readFile, writeFile } from 'node:fs/promises'
 
 const putDemo = async () => {
   const html = (await readFile('./index.html')).toString()
-  const replaced = html.replace(/\/demo.tsx/, '/demo.mjs')
+  const replaced = html.replace(/\/demo.tsx/, './demo.mjs')
 
   await writeFile('./dist/index.html', replaced)
 }
